829d57b218d8d563d7b07476f0fb772e4d5c63cc
“preetimca2016” <preetimca2016@gmail.com>
2022-08-29 00:25:29 +0530

    Add: Stack file

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Stack
+    {
+        LinkedList linkedList;
+        public Stack()
+        {
+            linkedList = new LinkedList();
+        }
+        public void Push(Node node)
+        {
+            linkedList.AddNode(node);
+        }
+        public void DisplayStack()
+        {
+            Console.WriteLine("Stack:");
+            linkedList.Display();
+        }
+        public void Top()
+        {
+            var val = linkedList.Head;
+            Console.WriteLine(val.data);
+        }
+        public void Pop()
+        {
+            linkedList.DeleteNodeAtFirst();
+        }
+    }
+}

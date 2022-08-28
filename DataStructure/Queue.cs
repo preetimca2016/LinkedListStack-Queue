de224da43e5e6e3e9f40062e1997e8a4c6892671
“preetimca2016” <preetimca2016@gmail.com>
2022-08-29 00:29:42 +0530

    Add: Queue File

---
+++ DataStructure/Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Queue
+    {
+        LinkedList linkedList;
+        public Queue()
+        {
+           linkedList = new LinkedList();
+        }
+        public void Enqueue(Node node)
+        {
+            linkedList.AppendNode(node);
+        }
+        public void DisplayQueue()
+        {
+            Console.WriteLine("Queue");
+            linkedList.Display();
+        }
+        public void Dequeue()
+        {
+            linkedList.DeleteNodeAtLast();
+        }
+    }
+}
